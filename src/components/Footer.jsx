d4de77605b26be7f304ba975bdeafac2a3d9fa76
Wyatt Barger <wyattbarger@icloud.com>
2023-11-05 16:16:08 -0600

    Changed the size of the bug icon in the footer so that it is smaller than the center links, as well as changed the color so it is a darker red. Added functionality to the share button so that the current pages url copies to the users clipboard when it is clicked. Issues getting share button to resize in line with the bug icon. Working towards mvp continues. Alert on the share button click is the last mvp feature for the footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,21 @@
-import { Footer } from 'flowbite-react'; // Add an import to include the footer component I will be using to customize for my application.
+// Add an import to include the footer component I will be using to customize for my application.
+import { Footer } from 'flowbite-react'; 
 // Add imports for the necessary SVG images from the assets directory so that they can be used in the footer.
 import Github from '../assets/github.svg'; 
 import Email from '../assets/mail.svg'; 
 import LinkedIn from '../assets/linkedin.svg'; 
 import Bug from '../assets/bug.svg'; 
-import Share from '../assets/sharing.svg'; 
+import Share from '../assets/sharing.svg';
+// Add a function that will copy the url of the current page to the users clip board.
+function controlC(e) {
+    e.preventDefault();
+    navigator.clipboard.writeText(window.location.href);
+};
 
 function MyFooter () {
     return (
-        <Footer container className='backgroundflex bg-black'>
-                <Footer.Brand
+        <Footer container className='backgroundflex bg-black rounded-none'>
+                <Footer.Brand id='bug-report-button' 
                 href="https://github.com/wyattbarger/react-portfolio-2023/issues"
                 src={Bug}
                 alt="Bug" 
@@ -31,14 +37,15 @@ function MyFooter () {
                 alt="LinkedIn Logo"
                 />
             </div>
-                <Footer.Brand
-                href=""
+                <Footer.Brand id='share-button' 
+                href="#"
+                onClick={controlC}
                 src={Share}
-                alt=""
+                alt="Share"
                 />
         </Footer>
     );
-}
+};
 
 export default MyFooter;
 
