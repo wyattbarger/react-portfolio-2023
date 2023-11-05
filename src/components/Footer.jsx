06447ee4283bcc5cdebe55a99da4fe3709a108fb
Wyatt Barger <wyattbarger@icloud.com>
2023-11-05 10:34:44 -0600

    Added the basic frame for the footer component I will be passing to App.jsx.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Footer } from 'flowbite-react'; // Add an import to include the footer component I will be using to customize for my application.
+
+function Footer () {
+    return (
+        <Footer container>
+
+        </Footer>
+    );
+}
+
+export default Footer;
+
+// Component Docs: 
+//
