7c3c5f4fdfcaca8c5830f6a8c5f9a1f0cc7cd61c
Wyatt Barger <wyattbarger@icloud.com>
2023-11-04 21:33:52 -0500

    Added the basic frame for what the profolio will be at the level of App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
-import { Button } from 'flowbite-react';
 
 function App() {
 
 
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-
+    <>
+    {/* Add the Header component to render the header for the portfolio. */}
+    <Header />
+      {/* Add the Outlet component to render the view of the portfolio the user is currently on. */}
+      <Outlet />
+    {/* Add the footer component to render the footer for the portfolio. */}
+    <Footer />
+  </>
   )
 
 }
