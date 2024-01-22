0606c25a480e5fc3f5d762f018224937aef889fe
Wyatt Barger <wyattbarger@icloud.com>
2024-01-22 10:44:37 -0600

    Added the appropriate tokens from EmailJS to the EmailButton component code obtained from their documentation.

--- src/components/EmailButton.jsx
+++ src/components/EmailButton.jsx
@@ -7,7 +7,7 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_8wydy76', 'template_yei6unq', form.current, '0Do3l4CleIcGIZG4D')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
