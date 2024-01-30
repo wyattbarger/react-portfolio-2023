3f4f35132ba929a61e7a590926e0558a8807be94
Wyatt Barger <wyattbarger@icloud.com>
2024-01-30 10:05:35 -0600

    Projects are successfully displaying the name, thumbnail, description, and the 'button' components are now linking to the proper locations. Box shadow set up inline with the rest of the portfolio styling, and mobile responsiveness has been tested and ensured.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,21 @@
+import React, { useState } from "react";
 import GithubButton from "./GithubButton";
 import DeploymentButton from "./DeploymentButton";
 
-function ProjectCard ({ projectName, projectThumbnail, projectLink, deploymentLink}) {
+function ProjectCard ({ projectName, projectThumbnail, projectDescription, projectLink, deploymentLink}) {
+    const [mouseHover, setMouseHover] = useState(false);
+
     return (
-        <section className="w-fit h-fit bg-black border border-cyan-700 rounded-lg">
+        <section className="sm:max-w-md mx-auto flex flex-col items-center justify-center w-fit h-fit bg-black border border-cyan-700 rounded-lg p-5 m-10"
+        onMouseEnter={() => setMouseHover(true)}
+        onMouseLeave={() => setMouseHover(false)}
+        style={{
+          boxShadow: mouseHover ? "0 0 100px #2B6CB0" : "none",
+          transition: "box-shadow 0.5s ease-in-out",
+        }}>
             <h3 className="mb-2 text-3xl font-bold text-cyan-700">{projectName}</h3>
-            <img src={projectThumbnail} alt="Thumbnail" /> 
+            <img className="" src={projectThumbnail} alt="Thumbnail" />
+            <p className="text-sm text-white font-montserrat mt-2">{projectDescription}</p>
             <GithubButton projectLink={projectLink}/>
             <DeploymentButton deploymentLink={deploymentLink}/>
         </section>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import ProjectCard from "../components/ProjectCard";
+import GamePopThumbnail from "../assets/LinkedIn-gamepop-sc.png";
+import ShinySpiderThumbnail from "../assets/readme-thumbnail-image.png";
+
 export default function Portfolio() {
   const [mouseHover, setMouseHover] = useState(false);
   const projects = [
     {
       projectName: "Game Pop!",
-      projectThumbnail: "",
+      projectThumbnail: GamePopThumbnail,
+      projectDescription: "A project designed and developed in collaboration with Andrew Mell and Alex Blaylock. This full stack application uses the MERN stack, as well as the IGDB API to create a game where users guess the which video game of two presented choices is the most popular. ",
       projectLink: "https://github.com/wyattbarger/gamepop-over-under",
       deploymentLink: "https://game-pop-0133abbe0985.herokuapp.com",
     },
     {
       projectName: "shiny-spider",
-      projectThumbnail: "",
+      projectThumbnail: ShinySpiderThumbnail,
+      projectDescription: "A Node project created using puppeteer to develop a plug and play web scraper that will scrape the names and stock prices of the S&P 500 Index and store them in an array of objects. Uses chalk and progress from npm for a clean visual representation of the scraping process.",
       projectLink: "https://github.com/wyattbarger/shiny-spider",
       deploymentLink: "https://www.npmjs.com/package/shiny-spider",
     },
@@ -19,14 +24,18 @@ export default function Portfolio() {
 
   return (
     <div
-      style={{ background: "linear-gradient(to top, black, #2a4365, black)" }}
-      className=""
+      style={{ background: "linear-gradient(to top, black, #2a4365, black)",
+      minHeight: "100vh"
+     }}
+      className="pt-10"
     >
-      <section className="">
+      <section className="p-10">
       {projects.map((project, index) => (
         <ProjectCard
           key={index}
           projectName={project.projectName}
+          projectThumbnail={project.projectThumbnail}
+          projectDescription={project.projectDescription}
           projectLink={project.projectLink}
           deploymentLink={project.deploymentLink}
         />
